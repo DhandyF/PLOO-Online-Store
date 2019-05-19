9cf30485655113c25a94041ab44b8957d92a83ba
Dhandyf <dhandy.friyadi@gmail.com>
2019-05-19 14:55:56 +0700

    Deploy to Heroku

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,34 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'product',
+                loadChildren: './product/product.module#JhipsterProductModule'
+            },
+            {
+                path: 'product-category',
+                loadChildren: './product-category/product-category.module#JhipsterProductCategoryModule'
+            },
+            {
+                path: 'customer',
+                loadChildren: './customer/customer.module#JhipsterCustomerModule'
+            },
+            {
+                path: 'product-order',
+                loadChildren: './product-order/product-order.module#JhipsterProductOrderModule'
+            },
+            {
+                path: 'order-item',
+                loadChildren: './order-item/order-item.module#JhipsterOrderItemModule'
+            },
+            {
+                path: 'invoice',
+                loadChildren: './invoice/invoice.module#JhipsterInvoiceModule'
+            },
+            {
+                path: 'shipment',
+                loadChildren: './shipment/shipment.module#JhipsterShipmentModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
